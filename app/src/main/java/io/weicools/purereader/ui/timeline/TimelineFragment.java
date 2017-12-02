8470d18e6801f1ca0500c2ceedc65391f5b7eb44
lecymeng <1570682285@qq.com>
2017-12-02 17:59:19 +0800

    impl mainUI

---
+++ app/src/main/java/io/weicools/purereader/ui/timeline/TimelineFragment.java
@@ -0,0 +1,33 @@
+package io.weicools.purereader.ui.timeline;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.weicools.purereader.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimelineFragment extends Fragment {
+
+
+    public TimelineFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_timeline, container, false);
+    }
+
+    public static TimelineFragment newInstance() {
+        return new TimelineFragment();
+    }
+}
