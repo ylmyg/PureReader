24c92298c6460aaf5c735a2a86f922295730c43f
lecymeng <1570682285@qq.com>
2017-12-02 17:58:43 +0800

    add App

---
+++ app/src/main/java/io/weicools/purereader/PureReaderApp.java
@@ -0,0 +1,23 @@
+package io.weicools.purereader;
+
+import android.app.Application;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatDelegate;
+
+/**
+ * Create by weicools on 2017/12/2.
+ * <p>
+ * Initialize some properties in the application such as the app theme.
+ */
+
+public class PureReaderApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean("night_mode", false)) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+}
