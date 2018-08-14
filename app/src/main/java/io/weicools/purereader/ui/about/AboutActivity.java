56387c07c5236ed29b2efe6507777bf31a642905
lecymeng <1570682285@qq.com>
2018-08-14 23:02:07 +0800

    add about page

---
+++ app/src/main/java/io/weicools/purereader/ui/about/AboutActivity.java
@@ -0,0 +1,14 @@
+package io.weicools.purereader.ui.about;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import io.weicools.purereader.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_about);
+  }
+}
